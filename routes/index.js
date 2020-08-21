30d5f05bcfa2622f68871f6d6f6ab717d87affda
pdemidyuk <pdemidyuk@infoblox.com>
2020-08-21 23:55:17 +0300

    refactoring

--- routes/index.js
+++ routes/index.js
@@ -144,6 +144,7 @@ router.get('/wifi/status', (_, res) => {
 router.post('/wifi/save', (req, res) => {
     exec('sed -i -E "s/ssid=\\"[^\\"]*\\"/ssid=\\"' + req.body.net + '\\"/g" /etc/wpa_supplicant/wpa_supplicant.conf', (err, stdout, stderr) => {
         exec('sed -i -E "s/psk=\\"[^\\"]*\\"/psl=\\"' + req.body.pass + '\\"/g" /etc/wpa_supplicant/wpa_supplicant.conf', (err, stdout, stderr) => {
+
             res.send({
                 status: 'ok'
             })
