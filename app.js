6bb8d1dd98e7377afb063b59906c76b4e7859492
Pavel Demidyuk <3128914@gmail.com>
2020-09-04 16:38:01 +0300

    refactoring

--- app.js
+++ app.js
@@ -12,13 +12,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.disable('etag');
+app.use(logger('dev'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
