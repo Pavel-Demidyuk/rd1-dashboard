3b2b60616da8da7db19c49be580b51fbffe3f7f4
Pavel Demidyuk <3128914@gmail.com>
2020-09-04 18:08:48 +0300

    refactoring

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use('/', indexRouter);
 app.disable('etag');
 
